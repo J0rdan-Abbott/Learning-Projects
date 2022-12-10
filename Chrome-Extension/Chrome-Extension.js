fae31e7b4e1099747659f2da76946627fc25bde7
J0rdan-Abbott <119947452+J0rdan-Abbott@users.noreply.github.com>
2022-12-10 18:23:12 +0000

    leads tracker commit
    original end-product

--- Chrome-Extension/Chrome-Extension.js
+++ Chrome-Extension/Chrome-Extension.js
@@ -2,21 +2,31 @@ const saveInputButton = document.getElementById('input-btn');
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const deleteButton = document.getElementById('delete-btn');
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const saveTab = document.getElementById('tab-btn');
 
-saveInputButton.addEventListener('click', () => {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    renderLeads();
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+};
+
+saveTab.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
 });
 
-function renderLeads() {
+function render(leads) {
     let listItems = "";
 
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a href="${myLeads[i]}" target="_blank">
-                    ${myLeads[i]}
+                <a href="${leads[i]}" target="_blank">
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -24,3 +34,18 @@ function renderLeads() {
 
     ulEl.innerHTML = listItems;
 };
+
+deleteButton.addEventListener('dblclick', () => {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
+
+saveInputButton.addEventListener('click', () => {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+    render(myLeads);
+});
